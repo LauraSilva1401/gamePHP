1f3a6a4d2327ff9953b14361d659f1f6f10147b7
Laura Silva <laura.c.silva01@gmail.com>
2023-03-11 18:52:22 -0500

    Login design updated

--- controllers/login.php
+++ controllers/login.php
@@ -1,4 +1,10 @@
 <?php 
+require_once '../models/user.php';
 
+$email = $_POST['Email'];
+$password = $_POST['Password'];
+
+$login = new User($email,$password);
+echo $login->validateData();
 
 ?>
--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
 	console.log("si entro al login");
+
+	$("#submit").click( function(event) {    
+        event.preventDefault();                             
+        var email = $("#email").val();
+        var password = $("#password").val();
+      
+
+        $.post("controllers/login.php",{Email: email, Password: password},function(res){
+            console.log("***"+res+"***");
+            if(res == true){
+            	console.log("entro si");
+               //window.location.href = "devices.php";   
+            }else{
+                console.log("entro no");
+            } 
+            
+            //$("#fracaso").delay(500).fadeIn("slow"); 
+             
+        })  
+    });
+
 });
