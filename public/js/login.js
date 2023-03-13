abf0408ab10ea677f1d5e06d1414b15608b4c5a2
Laura Silva <laura.c.silva01@gmail.com>
2023-03-13 12:35:27 -0400

    Added positive/negative warning messages to login -ls

--- public/js/login.js
+++ public/js/login.js
@@ -11,14 +11,50 @@ $(document).ready(function(){
             console.log("***"+res+"***");
             if(res == true){
             	console.log("entro si");
+                $("#PositiveAlert").show();
+                if ( !$( '#PositiveAlert' ).hasClass( 'yes' ) ){
+                    $( '#PositiveAlert' ).addClass( "yes" );
+                }
                //window.location.href = "devices.php";   
             }else{
                 console.log("entro no");
+                //$("#NegativeAlert").delay(500).fadeIn("slow"); 
+                var html = document.getElementById("NegativeAlert");
+                 html.innerHTML = "<strong>"+res+"</strong>";
+                $("#NegativeAlert").show();
+                if ( !$( '#NegativeAlert' ).hasClass( 'yes' ) ){
+                    $( '#NegativeAlert' ).addClass( "yes" );
+                }
+                validatedLoginForm(res);
             } 
             
             //$("#fracaso").delay(500).fadeIn("slow"); 
              
         })  
     });
+
+    function validatedLoginForm(ans){
+
+        if($("#email").val() == "" || ans === "Error, email length must be between 15 and 30"){
+            $("#email").focus();
+        }
+        if($("#password").val() == "" || ans === "Error, password length must be between 6 and 12"){
+            $("#password").focus(); 
+        }
+
+
+    }
+
+    setInterval(function () {
+        // and at here you can check whether the html coming is success or error
+        if ( $( '#NegativeAlert' ).hasClass( 'yes' ) ){
+          $( '#NegativeAlert' ).removeClass( "yes" );
+          $( '#NegativeAlert' ).hide();
+        }
+        if ( $( '#PositiveAlert' ).hasClass( 'yes' ) ){
+          $( '#PositiveAlert' ).removeClass( "yes" );
+          $( '#PositiveAlert' ).hide();
+        }
+     }, 5000);
 
 });
