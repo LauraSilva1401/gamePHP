f1ef7c89b1d6dde9ba159e5a550620caaa0d146c
Laura Silva <laura.c.silva01@gmail.com>
2023-03-13 11:49:21 -0400

    solved login error -ls

--- models/user.php
+++ models/user.php
@@ -159,6 +159,7 @@ class User
 
 		$query = "SELECT * FROM users WHERE userName ='".$this->email."';";
 	    $invokeQuery = $this->db->query($query);
+		
 
 	    if ($invokeQuery->num_rows==0){
 	    	//no user in db
