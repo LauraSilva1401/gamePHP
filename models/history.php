b7e417610186401323c99db116641f4c35f03035
jose salazar <joshrs23@gmail.com>
2023-03-27 13:07:40 -0400

    history so so

--- models/history.php
+++ models/history.php
@@ -5,10 +5,17 @@
 class History
 {
 	private $db;
-	private $text;
-	function __construct(argument)
+	private $result;
+	private $lives;
+	private $userId;
+	private $pass;
+
+	function __construct($result, $lives, $userId,$pass)
 	{
-		// code...
+		$this->result = $result;
+		$this->lives = $lives;
+		$this->userId = $userId;
+		$this->pass = $pass;
 	}
 
 	function validateDataDB(){
@@ -29,34 +36,55 @@ class History
 	function insertDataDB(){
 
 		//validate sqlInjection
-		$this->fname = mysqli_real_escape_string($this->db,$this->fname);
-		$this->lname = mysqli_real_escape_string($this->db,$this->lname);
-		$this->password = mysqli_real_escape_string($this->db,$this->password);
-		//password encryted
-		$this->password = base64_encode(password_hash($this->password, PASSWORD_BCRYPT, ["cost" => 10]));
+		$this->result = mysqli_real_escape_string($this->db,$this->result);
+		$this->lives = mysqli_real_escape_string($this->db,$this->lives);
+		$this->userId = mysqli_real_escape_string($this->db,$this->userId);
+		$this->pass = mysqli_real_escape_string($this->db,$this->pass);
 
-		$query = "INSERT INTO users (FirstName, LastName, userName, Password) VALUES ('".$this->fname."','".$this->lname."','".$this->email."','".$this->password."');";
+		if ($this->verifyUserBefore() == TRUE) {
+			$query = "INSERT INTO historypage (result, lives, userId) VALUES ('".$this->result."','".$this->lives."','".$this->userId."');";
 
-	    $invokeQuery = $this->db->query($query);
+		    $invokeQuery = $this->db->query($query);
 
-	    $this->db->close();
-	    
-	    if ($invokeQuery === FALSE){
-	    	//no user in db
-	    	return FALSE;
-	    }
-	    else{
-	    	//there is an user in the db
-	    	//start session variable
-	    	session_start();
-	    	$_SESSION['email'] = $this->email;
-	    	$_SESSION['name'] = $this->fname;
-	    	$_SESSION['password'] = $this->password;
-	    	$_SESSION['LOGIN_STATUS'] = true;
-	    	$_SESSION['lives'] = 6;
-	    	$_SESSION['level'] = 1;
-	    	return TRUE;
-	    } 
+		    $this->db->close();
+
+		    if ($invokeQuery === FALSE){
+		    	//no user in db
+		    	return FALSE;
+		    }
+		    else{
+		    	//there is an user in the db
+		    	//start session variable
+		    	//session_start();
+		    	$_SESSION['email'] = $this->email;
+		    	$_SESSION['name'] = $this->fname;
+		    	$_SESSION['password'] = $this->password;
+		    	$_SESSION['LOGIN_STATUS'] = true;
+		    	$_SESSION['lives'] = 6;
+		    	$_SESSION['level'] = 1;
+		    	return TRUE;
+		    }
+		}
+		
 	}
+
+	function verifyUserBefore(){
+
+		//validate sqlInjection
+		$query = "SELECT * FROM users WHERE userName ='".$this->userId."';";
+		$invokeQuery = $this->db->query($query);
+
+		$each_row = $invokeQuery->fetch_array(MYSQLI_ASSOC);
+
+		$pwd_peppered = base64_decode($each_row['Password']);
+
+		if (password_verify($this->pass,$pwd_peppered)) {
+			return TRUE;
+		}
+		else {
+		    return FALSE;
+		}
+	}
+
 }
 ?>
